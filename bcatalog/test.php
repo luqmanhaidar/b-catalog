24e6ab73420c2d38dbe2b14a59d35f0839bc737c
Grind.DH <Grind.DH@gmail.com>
2011-01-25 22:23:29 +0000

    index page cities db link

--- bcatalog/test.php
+++ bcatalog/test.php
@@ -6,17 +6,39 @@
 	</head>
 	<body>
         <?php
-//            require_once('source/config.php');
-//            require_once('models/bank.model.php');
-//
-//            $bank =  new Bank(null, $config, 3, "Alpha", "Alpha-Bank");
-//
-//            print_r(Bank::getAllBanks($bank->getDBH()));
+            try{
+                require_once(realpath(dirname(__FILE__))."/source/config.php");
+                require_once(realpath(dirname(__FILE__))."/source/db_connect.class.php");
+                require_once(realpath(dirname(__FILE__))."/source/helper.php");
+                require_once(MODEL_PATH."/bank.model.php");
+                require_once(MODEL_PATH."/region.model.php");
 
-//phpinfo();
+                $db = new DB_connect(null, $config);
 
-        $dbh=mysql_connect ("localhost", "ctrigge8_bcatuse", "hQ_3Fg7K") or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db ("ctrigge8_bankcatalog");
+                $data["title"] = "Каталог банков";
+                $data["banks"] = Bank::getBanksShortInfo($db->getDBH());
+                $data["regions"] = Region::getAllRegions($db->getDBH());
+
+                echo "<pre>";
+                print_r($data["regions"]);
+                echo "</pre>";
+                //renderView("main.view.php", $data);
+            }
+            catch(Exception $exp){
+                // log exception info
+                $log = fopen(LOG_PATH."/err.log", "a");
+                $report = "-----\n".
+                          "ERROR:\n".
+                          $exp->getMessage()."\n".
+                          "in: ".$exp->getFile()." at: ".$exp->getLine()."\n".
+                          "trace: \n".$exp->getTraceAsString()."\n".
+                          "-----\n\n";
+
+                fwrite($log, $report);
+                fclose($log);
+
+                //renderView("error.view.php", array("title"=>"Ошибка сервера", "msg"=>"Приносим свои извинения.<br/>Попробуйте перезагрузить страницу."));
+            }
         ?>
     </body>
 </html>
