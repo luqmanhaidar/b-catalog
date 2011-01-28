6b18684c3c8fe5e6221b2a06837a6bdde41d571a
Grind.DH <Grind.DH@gmail.com>
2011-01-28 08:00:35 +0000

    new error | zombie-city

--- bcatalog/source/config.php
+++ bcatalog/source/config.php
@@ -31,6 +31,7 @@ $config = array(
         "data" => array(
             "no-ness" => '{"success":"0", "error":"1", "notification":"в обработчик не были переданы все необходимые данные"}',
             "wrong-cmd" => '{"success":"0", "error":"1", "notification":"в обработчик была передана неверная команда"}',
+            "zombie-city" => '{"success":"0", "error":"1", "notification":"в этом городе нет известных нам отделений"}',
             "temp" => '{"success":"1", "notification":"временная заглушка"}'
         )
     )
