3e72324d718bf3ab061b2ea7337e7c1cb40c3cf9
Grind.DH <Grind.DH@gmail.com>
2011-02-03 15:13:15 +0000

    work hours format conversion

--- bcatalog/source/helper.php
+++ bcatalog/source/helper.php
@@ -56,13 +56,66 @@ function ifEmptyGetFromConfig($config, $configTitle, $valueToTest, $numeric = fa
     if(!$configTitle)
         return false;
 
-    if(empty($valueToTest)){
-        if($numeric && !is_numeric($valueToTest))
-            return false;
-        else
-            return $config[$configTitle];
+    if(empty($valueToTest))
         return $config[$configTitle];
+    else if($numeric && !is_numeric($valueToTest))
+        return false;
+    else
+        return $valueToTest;
+}
+
+/*
+ * тестит данные на соответствие формату:
+ * <div class="days">пн-пт. :</div><div class="hours">9:00-18:00</div>
+ * <div class="days">сб-вс. :</div><div class="hours">выходной</div>
+ *
+ * если НЕ совпадает считает обычной строкой и пытается распарсить РВ,
+ * затем вернуть в приведенном формате
+ *
+ * @param {String} $strToProcess строка для проверки
+ */
+function processWorkHoursToHRD($strToProcess, $deptType = 1, $deptWorkDays = "123456"){
+
+    $resultArr = array();
+    $result = "";
+
+    if(strstr($strToProcess, "<div"))
+        return $strToProcess;
+    
+    if(preg_match_all("/( ?([а-я]{2}\.(,| )?)(.? ?([а-я]{2}\.(,| )?))?)(([a-zа-я:] ([0-9]+\.[0-9]{2}).{3,4}([0-9]+\.[0-9]{2})( без обеда)?)|(и праздничные дни.{3}выходной))/iu", $strToProcess, $resultArr)){
+        for( $q= 0; $q < count($resultArr[1]); $q++){
+            $result .= ("<div class='days'>".str_replace(" ", "", str_replace(",","-",$resultArr[1][$q])).":</div>");
+            $result .= ("<div class='hours'>".str_replace("без обеда","", preg_replace("/^:|(и праздничные дни.{3})|(без обеда)| |c|с|/iu", "", str_replace("до","-",trim($resultArr[7][$q]))))."</div>");
+        }
+        if(preg_match_all("/(((обед( и технические перерывы)?)|(технические перерывы)):( ?([0-9]+\.[0-9]{2}).{3,4}([0-9]+\.[0-9]{2})(,|;)?)+)/iu", $strToProcess, $resultArr)){
+
+            $resultArr = preg_split("/;|,/", preg_replace("/(обед)|(технические перерывы)| |и|:/iu", "", $resultArr[0][0]));
+
+            $result .= "<div class='days'>обед и перерывы:</div><div class='hours'><ul>";
+            for($q = 0; $q < count($resultArr); $q++)
+                $result .= "<li>".$resultArr[$q]."</li>";
+            $result .= "</ul></div>";
+        }
     }
+    else if(preg_match_all("/(([0-9]+\.[0-9]{2}).{3,4}([0-9]+\.[0-9]{2}))/iu", $strToProcess, $resultArr)){
+
+        $result .= ("<div class='days'>пн.-вс.:</div>");
+        $result .= ("<div class='hours'>".str_replace(" ", "", $resultArr[0][0])."</div>");
+
+        if(preg_match_all("/(((обед( и технические перерывы)?)|(технические перерывы)):( ?([0-9]+\.[0-9]{2}).{3,4}([0-9]+\.[0-9]{2})(,|;)?)+)/iu", $strToProcess, $resultArr)){
+
+            $resultArr = preg_split("/;|,/", preg_replace("/(обед)|(технические перерывы)| |и|:/iu", "", $resultArr[0][0]));
+
+            $result .= "<div class='days'>обед и перерывы:</div><div class='hours'><ul>";
+            for($q = 0; $q < count($resultArr); $q++)
+                $result .= "<li>".$resultArr[$q]."</li>";
+            $result .= "</ul></div>";
+        }
+    }
+    else
+        return "<div class='hours'>".$strToProcess."</div>";
+
+    return $result;
 }
 
 ?>
