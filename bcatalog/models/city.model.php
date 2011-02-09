cbad4f832e45510ebda18311dbdbf8968418f1c5
Grind.DH <Grind.DH@gmail.com>
2011-02-09 13:04:01 +0000

    bug fixes

--- bcatalog/models/city.model.php
+++ bcatalog/models/city.model.php
@@ -17,43 +17,24 @@ class City extends DB_connect{
         $this->name = empty ($name) ? NULL : $name;
 
     }
-
-
-//    public static function getAreaCities(PDO $dbh, int $area_id){
-//
-//        if(empty($area_id) || !is_object($dbh))
-//            return false;
-//
-//        $query = "SELECT city_id, name FROM cities WHERE area_id=$area_id";
-//        $sth = $dbh->query($query);
-//        $sth->setFetchMode(PDO::FETCH_ASSOC);
-//
-//
-//        $cities = array();
-//        while($row = $sth->fetch())
-//            $cities[] = new City($dbh, NULL, $row["city_id"], $area_id, $city["name"]);
-//
-//        return $cities;
-//    }
+    
 
     public function add(){
 
         $query = "INSERT INTO cities (area_id, name) VALUES ($this->area_id, '$this->name')";
 
-        $this->db->prepare($query);
-        $this->db->execute();
+        $sth = $this->db->prepare($query);
+        $sth->execute();
 
         return $this->db->lastInsertId();
     }
 
     public function update(){
 
-        $query = "UPDATE cities SET (area_id=$this->area_id, name='$this->name') WHERE city_id=$this->city_id";
+        $query = "UPDATE cities SET area_id=$this->area_id, name='$this->name' WHERE city_id=$this->city_id";
 
-        $this->db->prepare($query);
-        $this->db->execute();
-
-        return true;
+        $sth = $this->db->prepare($query);
+        return $sth->execute();
     }
     
 
@@ -61,10 +42,8 @@ class City extends DB_connect{
 
         $query = "DELETE FROM cities WHERE city_id=$this->city_id";
 
-        $this->db->prepare($query);
-        $this->db->execute();
-
-        return true;
+        $sth = $this->db->prepare($query);
+        return $sth->execute();
     }
 
 
