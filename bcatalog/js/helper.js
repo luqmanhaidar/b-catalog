1617a8a1f4b7cb1f8e235e0d68bae88a7ed85b9a
Grind.DH <Grind.DH@gmail.com>
2011-02-13 14:57:38 +0000

    empty func

--- bcatalog/js/helper.js
+++ bcatalog/js/helper.js
@@ -33,3 +33,8 @@ Helper.getInnerHTMLToArr = function(donorElts){
 
     return innerHTMLArr;
 }
+
+Helper.empty = function empty( mixed_var ) {
+
+	return ( mixed_var === "" || mixed_var === 0   || mixed_var === "0" || mixed_var === null  || mixed_var === false  ||  ( is_array(mixed_var) && mixed_var.length === 0 ) );
+}
